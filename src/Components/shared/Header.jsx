aae5afadbc123e47a005a816f20afd9611307eb2
jamal-uddin-asif <asifzehendmg@gmail.com>
2026-07-27 23:23:23 +0600

    Color select for application

--- src/Components/shared/Header.jsx
+++ src/Components/shared/Header.jsx
@@ -1,31 +1,34 @@
 import React from "react";
 import Container from "./Container";
 import { AiFillMedicineBox } from "react-icons/ai";
+import { LuScanSearch } from "react-icons/lu";
 
 const Header = () => {
   return (
-    <div>
+    <div className="bg-background">
       <Container>
-        <div className=" border p-8 flex justify-between items-center">
+        <div className="  p-8 md:flex justify-between items-center">
             {/* left */}
           <div>
-            <h1 className="uppercase text-2xl flex items-center font-bold border">
-              <AiFillMedicineBox size={40} color="maroon" />
-              <span className="text-green-500">Chanua</span>
-              <span className="text-red-600">Care</span>
+            <h1 className="uppercase text-primary  text-2xl flex items-center font-bold ">
+              <AiFillMedicineBox size={40} color="" />
+              <span className="">Chanua</span>
+              <span className="text-secondary">Care</span>
             </h1>
           </div>
           {/* center */}
-          <div>
+          <div className="relative">
             <input 
             type="text"
-             placeholder="Search your medicin"
-             className="bg-[#f1f1f1f1] py-2 px-3 rounded-full min-w-90 pl-22"
+             placeholder="Search your medicine"
+             className="bg-[#f1f1f1f1] text-text active:bg-surface outline-border py-2 px-3 rounded-full min-w-90 pl-22"
               />
+              <div className="absolute top-2.5 left-7"><LuScanSearch size={20}/></div>
           </div>
           {/* end */}
-          <div>
-            <button className="bg-amber-500 p-2 ">Login</button>
+          <div >
+            <button className="bg-amber-500 p-2 mr-2">Login</button>
+            <button className="bg-amber-500 p-2 ">Get start</button>
           </div>
         </div>
       </Container>
