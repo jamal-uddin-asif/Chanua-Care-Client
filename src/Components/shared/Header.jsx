4eee739c4ea425eaf3a1805b9f978a8ea8ab628a
jamal-uddin-asif <asifzehendmg@gmail.com>
2026-07-28 20:15:27 +0600

    Login Get start button style added

--- src/Components/shared/Header.jsx
+++ src/Components/shared/Header.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Container from "./Container";
 import { AiFillMedicineBox } from "react-icons/ai";
 import { LuScanSearch } from "react-icons/lu";
-import { NavLink } from "react-router";
 import NavberLink from "./NavberLink";
 import {
   FaHome,
@@ -48,7 +47,7 @@ const Header = () => {
     <>
       <div className="bg-background">
         <Container>
-          <div className="  p-8  flex flex-col md:flex-row justify-between items-center">
+          <div className="  p-8  flex  md:flex-row justify-between items-center">
             {/* left */}
             <div>
               <h1 className="uppercase text-primary  text-2xl flex items-center font-bold ">
@@ -58,26 +57,26 @@ const Header = () => {
               </h1>
             </div>
             {/* center */}
-            <div className="relative">
+            <div className="relative hidden md:block">
               <input
                 type="text"
                 placeholder="Search your medicine"
                 className="bg-[#f1f1f1f1] text-text active:bg-surface outline-border py-2 px-3 rounded-full min-w-90 pl-22"
               />
               <div className="absolute top-2.5 left-7">
-                <LuScanSearch size={20} />
+                <LuScanSearch color="green" size={20} />
               </div>
             </div>
             {/* right */}
             <div>
-              <button className="bg-amber-500 p-2 mr-2">Login</button>
-              <button className="bg-amber-500 p-2 ">Get start</button>
+              <button className="border-2 border-border rounded-xl hover:bg-secondary p-2 font-bold hover:text-surface mr-2">Login</button>
+              <button className="bg-secondary hover:bg-transparent hover:text-text border-2 rounded-xl border-border p-2 text-surface font-bold">Get Start</button>
             </div>
           </div>
         </Container>
       </div>
       {/* nav */}
-      <nav className="bg-primary">
+      <nav className="bg-primary hidden md:block">
         <Container>
           <ul className="flex justify-center gap-8 py-2 text-surface">
             {links}
